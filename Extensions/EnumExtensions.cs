016cadd164ba0facf5ceac8b616d88513dc0554b
NigAday ... <ivan_mustafin@vk.com>
2026-07-11 16:23:17 +0300

    Settings and enum value localization refactoring
    - Moved settings and enum localization strings to UiSettingsResources.resx
    - Removed obsolete Settings.resx, EnumResources.resx, and .Designer files
    - Replaced all resource calls with new ones
    - Added a universal method for getting the localization manager by type
    - Introduced EnumExtensions for getting the DisplayAttribute from an enum
    - Implemented EnumView<TEnum> for localized dropdowns by enum
    - New language selection dropdown in Settings, two-way binding
    - Changed the default language to English
    - Refactored language handling methods
    - Updated subproject references, removed unnecessary resources from csproj

---
+++ Extensions/EnumExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace ObjectExtensions;
+
+public static class EnumExtensions
+{
+    public static DisplayAttribute? GetDisplayAttribute(this Enum value)
+    {
+        var member = value.GetType()
+            .GetMember(value.ToString())
+            .FirstOrDefault();
+
+        return member?.GetCustomAttribute<DisplayAttribute>();
+    }
+}
